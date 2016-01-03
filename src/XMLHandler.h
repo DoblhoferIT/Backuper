3f20b0c2fffe47a498c9e6853cb068a68c004f7a
Doblhofer Philipp <philipp@doblhofer.it>
2016-01-03 23:10:15 +0100

    Implemented Filesize, total filesize for folders, started with XML read/write

---
+++ src/XMLHandler.h
@@ -0,0 +1,39 @@
+/* XMLHandler.h
+ *
+ * Copyright (C) 2016 Doblhofer Philipp <philipp@doblhofer.it>
+ *
+ * This file is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This file is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef _XMLHANDLER_H_
+#define _XMLHANDLER_H_
+
+#include <libxml/xmlreader.h>
+#include <libxml/xmlwriter.h>
+#include <libxml/encoding.h>
+#include <string>
+#include "Folder.h"
+
+using namespace std;
+
+namespace DoblhoferIT {
+
+  class XMLHandler{
+  public:
+    // TODO: change to bool
+    static void writeXML(string xmlfilename, Folder *root);
+  };
+};
+
+#endif
